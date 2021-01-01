39cbf61ced49c8c70d0c62a710db0da9274dd2f7
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-01 17:55:01 +0530

    navigation component added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav class="navigation">
+        <div class="logo-container">
+          <a href="/">
+            <img
+              class="logo"
+              src="https://www.raweng.com/v3/assets/bltaacb6b0c9b693c2d/bltc1a71705f49d391d/5de663548d1dce6ad6bed8bf/header-logo.svg?format=pjpg&width=220"
+              alt=""
+            />
+          </a>
+        </div>
+        <div class="navigation-container">
+          <ul>
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#blog">
+                Blogs
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../blog-frontend/createBlog.html">
+                Create Blog <i class="fa fa-plus" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/logout" id="logout">
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <label id="switch" class="switch">
+                <input type="checkbox" id="slider" />
+                <span class="slider round"></span>
+              </label>
+            </li>
+          </ul>
+          <div id="ham">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -4,11 +4,16 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 //pages
 import Home from "../pages/Home";
 
+//components
+import Navigation from "../components/Navbar";
+
 export default class Router extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
+          <Navigation />
+
           <Switch>
             <Route path="/" exact component={Home} />
           </Switch>

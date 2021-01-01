b038bafbb1927df474f934a40180831109348ddf
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-01 21:04:23 +0530

    blog card implemented

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Card, CardText, CardBody, CardLink, CardTitle } from "reactstrap";
+
+export default class BlogCard extends Component {
+  render() {
+    const blog = this.props.blog;
+    return (
+      <div>
+        <Card>
+          <CardBody>
+            <CardTitle tag="h5">{blog.title}</CardTitle>
+          </CardBody>
+          <img width="100%" src={blog.imageUrl} alt={blog.title} />
+          <CardBody>
+            <CardText>{blog.content.substr(0, 100)}...</CardText>
+            <CardLink href="#">Card Link</CardLink>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}

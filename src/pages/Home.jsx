91d994e0570334bd22e665ff728541b213f70ac6
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-01 19:28:15 +0530

    card added for testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 
 export default class Home extends Component {
   render() {
+    const style = {
+      width: "100%",
+    };
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="body-container">
+        <div className="card-container">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="Avatar"
+            className="image"
+            style={style}
+          />
+          <div class="middle">
+            <div class="text">John Doe</div>
+          </div>
+        </div>
       </div>
     );
   }

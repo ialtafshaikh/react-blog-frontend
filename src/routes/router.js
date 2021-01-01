1ab6d69efcfd1ce0b0d434f7d13f6a2c2fd62dd1
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-01 17:21:53 +0530

    router enclosed to app and app.css and index css removed and home page added to route

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //pages
+import Home from "../pages/Home";
 
 export default class Router extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Switch>{/* <Route path="/" exact component={Todo} /> */}</Switch>
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
         </BrowserRouter>
       </div>
     );

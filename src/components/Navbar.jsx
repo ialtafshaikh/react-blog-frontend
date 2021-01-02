675cb8221ec8b4771df12cc44180332892e0dad0
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-02 10:41:27 +0530

    navbar render based on login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,48 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-function Navbar() {
+function Navbar(props) {
   return (
     <header>
-      <nav class="navigation">
-        <div class="logo-container">
+      <nav className="navigation">
+        <div className="logo-container">
           <a href="/">
             <img
-              class="logo"
+              className="logo"
               src="https://www.raweng.com/v3/assets/bltaacb6b0c9b693c2d/bltc1a71705f49d391d/5de663548d1dce6ad6bed8bf/header-logo.svg?format=pjpg&width=220"
               alt=""
             />
           </a>
         </div>
-        <div class="navigation-container">
+        <div className="navigation-container">
           <ul>
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#blog">
-                Blogs
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../blog-frontend/createBlog.html">
-                Create Blog <i class="fa fa-plus" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/logout" id="logout">
-                <i class="fa fa-sign-out" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <label id="switch" class="switch">
-                <input type="checkbox" id="slider" />
-                <span class="slider round"></span>
-              </label>
-            </li>
+            {props.isLoggedIn ? (
+              <div style={{ display: "inline" }} className="login-content">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Blogs
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/create">
+                    Create Blog{" "}
+                    <i className="fa fa-plus" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li className="nav-item" onClick={props.logout}>
+                  <a className="nav-link" href="##" id="logout">
+                    <i className="fa fa-sign-out" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </div>
+            ) : (
+              <li></li>
+            )}
           </ul>
           <div id="ham">
             <span></span>

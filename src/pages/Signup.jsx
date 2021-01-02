7d8b7d6c0eaf06de2ccc584153c58b862dd5ca9c
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-02 10:40:33 +0530

    signup page fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Navigation from "../components/Navbar";
+
 import { signup } from "../endpoints";
 
 export default class Signup extends Component {
@@ -38,87 +40,95 @@ export default class Signup extends Component {
   };
   render() {
     return (
-      <div className="form-container">
-        <h1 className="form-title">User Registration Form</h1>
-        <p className="error">{this.state.signupError}</p>
+      <div>
+        <Navigation isLoggedIn={false} />
+        <div className="form-container">
+          <h1 className="form-title">User Registration Form</h1>
+          <p className="error">{this.state.signupError}</p>
 
-        <form
-          action=""
-          method="post"
-          name="signup"
-          encType="application/x-www-form-urlencoded"
-          onSubmit={this.signup}
-        >
-          <label htmlFor="firstName">
-            <b>First Name</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter First Name"
-            name="firstName"
-            required
-          />
-          <br />
+          <form
+            action=""
+            method="post"
+            name="signup"
+            encType="application/x-www-form-urlencoded"
+            onSubmit={this.signup}
+          >
+            <label htmlFor="firstName">
+              <b>First Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter First Name"
+              name="firstName"
+              required
+            />
+            <br />
 
-          <label htmlFor="lastName">
-            <b>Last Name</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Last Name"
-            name="lastName"
-            required
-          />
-          <br />
+            <label htmlFor="lastName">
+              <b>Last Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Last Name"
+              name="lastName"
+              required
+            />
+            <br />
 
-          <label htmlFor="email">
-            <b>Email</b>
-          </label>
-          <input type="text" placeholder="Enter Email" name="email" required />
-          <br />
+            <label htmlFor="email">
+              <b>Email</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              required
+            />
+            <br />
 
-          <label htmlFor="username">
-            <b>Username</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Username"
-            name="username"
-            required
-          />
-          <br />
+            <label htmlFor="username">
+              <b>Username</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              name="username"
+              required
+            />
+            <br />
 
-          <label htmlFor="password">
-            <b>
-              Password: **atleast 1 cap, 1 number, 1 special symbol, total 8
-              letter
-            </b>
-          </label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            name="password"
-            required
-          />
-          <br />
+            <label htmlFor="password">
+              <b>
+                Password: **atleast 1 cap, 1 number, 1 special symbol, total 8
+                letter
+              </b>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              required
+            />
+            <br />
 
-          <label htmlFor="confirmPassword">
-            <b>Confirm Password</b>
-          </label>
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            required
-          />
-          <br />
-          <input type="submit" className="signupbtn" value="Sign Up" />
-          <hr />
-          <p>
-            Already have an account?
-            <a href="/">Sign in</a>.
-          </p>
-        </form>
+            <label htmlFor="confirmPassword">
+              <b>Confirm Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              required
+            />
+            <br />
+            <input type="submit" className="signupbtn" value="Sign Up" />
+            <hr />
+            <p>
+              Already have an account?
+              <a href="/">Sign in</a>.
+            </p>
+          </form>
+        </div>
       </div>
     );
   }

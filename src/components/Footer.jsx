2c66db4b337ef0cfd283563cedb7b7acd1fa9e13
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-02 10:41:47 +0530

    footer fixed with jsx warnings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,26 +3,34 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <p class="copy">
+      <p className="copy">
         &copy; 2020 All Rights Reserved by <span>Altaf Shaikh</span>
       </p>
-      <div class="social-links">
+      <div className="social-links">
         <a
           href="https://mobile.twitter.com/ialtafshaikh"
-          class="fa fa-twitter twitter"
-        ></a>
+          className="fa fa-twitter twitter"
+        >
+          {" "}
+        </a>
         <a
           href="https://www.linkedin.com/in/ialtafshaikh/"
-          class="fa fa-linkedin linkedin"
-        ></a>
+          className="fa fa-linkedin linkedin"
+        >
+          {" "}
+        </a>
         <a
           href="https://www.instagram.com/ialtafshaikh"
-          class="fa fa-facebook facebook"
-        ></a>
+          className="fa fa-facebook facebook"
+        >
+          {" "}
+        </a>
         <a
           href="https://github.com/altafshaikh"
-          class="fa fa-github github"
-        ></a>
+          className="fa fa-github github"
+        >
+          {" "}
+        </a>
       </div>
     </footer>
   );

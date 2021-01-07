4def1fae23cbfbc5fe0f31832ce64f756c43936f
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-07 14:07:25 +0530

    separated login from home and added login route and global state using redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,10 @@ import React, { Component } from "react";
 import Cookies from "js-cookie";
 import { Row, Col } from "reactstrap";
 
-import { endpoint, login } from "../endpoints";
+import { endpoint } from "../endpoints";
 
-import Login from "./Login";
 import BlogCard from "../components/BlogCard";
 import Navbar from "../components/Navbar";
-
-import store from "../redux/store/store";
-import { userActionTypes } from "../redux/constants/usersAction.types";
-import usersActionCreator from "../redux/actions/usersAction.creator";
 
 export default class Home extends Component {
   constructor() {
@@ -18,10 +13,8 @@ export default class Home extends Component {
     this.state = {
       blogs: [],
       isLoggedIn: false,
-      email: "",
-      password: "",
-      loginError: "",
       currentUser: {},
+      loading: true,
     };
   }
   componentDidMount = () => {
@@ -63,81 +56,19 @@ export default class Home extends Component {
       });
   };
 
-  setEmail = (event) => {
-    this.setState({ email: event.target.value });
-  };
-
-  setPassword = (event) => {
-    this.setState({ password: event.target.value });
-  };
-  setTaskName = (event) => {
-    this.setState({ taskName: event.target.value });
-  };
-  login = (event) => {
-    event.preventDefault();
-
-    if (this.state.email === "" && this.state.password === "") {
-      this.setState({ loginError: "email and password field empty" });
-      return;
-    }
-    if (this.state.email === "") {
-      this.setState({ loginError: "email field empty" });
-      return;
-    }
-
-    if (this.state.password === "") {
-      this.setState({ loginError: "password field empty" });
-      return;
-    }
-
-    var formData = new FormData(event.target);
-
-    var formObject = {};
-    formData.forEach(function (value, key) {
-      formObject[key] = value;
-    });
-
-    store.dispatch(
-      usersActionCreator(userActionTypes.LOGIN_SUCCESS, {
-        formObject: JSON.stringify(formObject),
-      })
-    );
-
-    // fetch(login, {
-    //   method: "POST", // or 'PUT'
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(formObject),
-    // })
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     if (data.status.status === "unsuccessful") {
-    //       this.setState({ loginError: data.status.message });
-    //       return;
-    //     }
-    //     Cookies.set("jwt", data.data[0]["jwt"]);
-    //     this.setState({ isLoggedIn: true });
-    //     this.loadBlogs();
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error:", error);
-    //   });
-  };
-
   logout = (event) => {
-    Cookies.remove("jwt");
-    this.setState({ isLoggedIn: false });
-    this.props.history.push("/");
+    // Cookies.remove("jwt");
+    // this.setState({ isLoggedIn: false });
+    // this.props.history.push("/");
   };
 
   render() {
     return (
       <div>
-        {this.state.isLoggedIn ? (
-          <div>
+        {this.state.loading ? (
+          <div>Loading..</div>
+        ) : (
+          <>
             <Navbar isLoggedIn={this.state.isLoggedIn} logout={this.logout} />
             <div className="body-container">
               <Row>
@@ -150,19 +81,7 @@ export default class Home extends Component {
                 })}
               </Row>
             </div>
-          </div>
-        ) : (
-          <div>
-            <Navbar isLoggedIn={this.state.isLoggedIn} />
-            <Login
-              login={this.login}
-              setEmail={this.setEmail}
-              email={this.state.email}
-              password={this.state.password}
-              setPassword={this.setPassword}
-              error={this.state.loginError}
-            />
-          </div>
+          </>
         )}
       </div>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,100 @@
 import React, { Component } from "react";
 
+import Navbar from "../components/Navbar";
+
+import store from "../redux/store/store";
+import { userActionTypes } from "../redux/constants/usersAction.types";
+import usersActionCreator from "../redux/actions/usersAction.creator";
+
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loginError: "",
+      // currentUser: {},
+    };
+  }
+
+  setEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  setPassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  login = (event) => {
+    event.preventDefault();
+
+    if (this.state.email === "" && this.state.password === "") {
+      this.setState({ loginError: "email and password field empty" });
+      return;
+    }
+    if (this.state.email === "") {
+      this.setState({ loginError: "email field empty" });
+      return;
+    }
+
+    if (this.state.password === "") {
+      this.setState({ loginError: "password field empty" });
+      return;
+    }
+
+    var formData = new FormData(event.target);
+
+    var formObject = {};
+    formData.forEach(function (value, key) {
+      formObject[key] = value;
+    });
+
+    store.dispatch(
+      usersActionCreator(userActionTypes.LOGIN_SUCCESS, {
+        formObject: JSON.stringify(formObject),
+      })
+    );
+  };
+
   render() {
     return (
-      <div className="form-container">
-        <h1 className="form-title">User Login Form</h1>
-        <p className="error">{this.props.error}</p>
-        <form
-          action=""
-          method=""
-          name="login"
-          encType="application/x-www-form-urlencoded"
-          onSubmit={this.props.login}
-        >
-          <label htmlFor="email">Email </label>
-          <input
-            type="email"
-            name="email"
-            onChange={this.props.setEmail}
-            value={this.props.email}
-          />
-          <br />
-          <label htmlFor="password">Password </label>
-          <input
-            type="password"
-            name="password"
-            onChange={this.props.setPassword}
-            value={this.props.password}
-          />
-          <br />
-          <input type="submit" className="loginbtn" value="Login" />
-          <hr />
-          <p>
-            don't have an account?
-            <a href="/signup">SignUp Here</a>.
-          </p>
-        </form>
-      </div>
+      <>
+        <Navbar />
+        <div className="form-container">
+          <h1 className="form-title">User Login Form</h1>
+          {/* <p className="error">{this.props.error}</p> */}
+          <form
+            action=""
+            method=""
+            name="login"
+            encType="application/x-www-form-urlencoded"
+            onSubmit={this.login}
+          >
+            <label htmlFor="email">Email </label>
+            <input
+              type="email"
+              name="email"
+              onChange={this.setEmail}
+              value={this.state.email}
+            />
+            <br />
+            <label htmlFor="password">Password </label>
+            <input
+              type="password"
+              name="password"
+              onChange={this.setPassword}
+              value={this.state.password}
+            />
+            <br />
+            <input type="submit" className="loginbtn" value="Login" />
+            <hr />
+            <p>
+              don't have an account?
+              <a href="/signup">SignUp Here</a>.
+            </p>
+          </form>
+        </div>
+      </>
     );
   }
 }

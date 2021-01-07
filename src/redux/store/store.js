3074791d99951a67d0a3888b5bae49dac6720e4b
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-07 20:12:01 +0530

    implemented the auth with redux

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
+// reducer
 import rootReducer from "../reducers/root.reducer";
 
 const store = createStore(
